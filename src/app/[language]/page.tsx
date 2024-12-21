2c81494da037844393d5f64a0daf4f0c6894b623
amitwaghmaretorinit <79451605+amitwaghmaretorinit@users.noreply.github.com>
2024-12-22 00:31:36 +0530

    minor changes

--- src/app/[language]/page.tsx
+++ src/app/[language]/page.tsx
@@ -1,22 +1,30 @@
-import HomePageSection from '@/components/PageContent'
-import { sanityFetch } from '@/sanity/lib/live'
-import { PAGE_QUERY } from '@/sanity/lib/queries'
-import { QueryParams } from 'next-sanity'
+import { QueryParams } from "next-sanity";
 
-export default async function Home ({
-  params
+import Header from "@/components/Header";
+import PageContent from "@/components/PageContent";
+import VisualEditWrapper from "@/components/VisualEditWrapper";
+import { sanityFetch } from "@/sanity/lib/live";
+import { PAGE_QUERY } from "@/sanity/lib/queries";
+
+export default async function Home({
+  params,
 }: {
-  params: Promise<QueryParams>
+  params: Promise<QueryParams>;
 }) {
-  const { language, sub_page } = await params
-  const path = sub_page ? `/${sub_page}` : '/'
+  const { language, sub_page } = await params;
+  const path = sub_page ? `/${sub_page}` : "/";
 
   const { data } = await sanityFetch({
     query: PAGE_QUERY,
     params: {
-      language: language || 'en',
-      path
-    }
-  })
-   return <HomePageSection {...data} />
+      language: language || "en",
+      path,
+    },
+  });
+  return (
+    <VisualEditWrapper id={data._id} type={data._type} path="page">
+      <Header />
+      <PageContent {...data} key={data._id} />
+    </VisualEditWrapper>
+  );
 }
